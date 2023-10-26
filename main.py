8da2e07dcd6dc9ab0f8e11c792823cf0544a40ae
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2023-10-26 02:45:30 +0000

    production

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from os import environ as env
+
 app = FastAPI()
 
 
 @app.get("/")
 def index():
-    return {"details": "Hello World!"}
+    return {"details": f"Hello World! Secret = {env['MY_VARIABLE']}"}
